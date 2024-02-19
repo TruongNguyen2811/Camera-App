826adb0ef584ec21d13532e9f32418ea19ecb9b7
truongnguyen <truongnguyen@aggregatoricapaci.com>
2024-02-19 21:54:42 +0700

    [update][domain][update domain prod]

--- lib/service/api/app_client.dart
+++ lib/service/api/app_client.dart
@@ -27,7 +27,7 @@ class AppClient {
 
   void _setupClient() {
     dio = Dio(BaseOptions(
-        baseUrl: "https://defender.machinax.net:8000",
+        baseUrl: "https://defender.machinax.net",
         receiveTimeout: 60 * 1000 * 2,
         connectTimeout: 60 * 1000 * 2,
         contentType: "application/json"));
